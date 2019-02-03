86fa17f4c57033d4c8ddcebe13f40c9b94a6c5a4
RajinTech <rajsglass@gmail.com>
2019-02-03 14:37:01 -0500

    added some maps

--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import ListingIndexContainer from '../containers/ListingIndexContainer'
+import MapContainer from '../containers/MapContainer'
 import ListingShowContainer from '../containers/ListingShowContainer'
 import NavBar from '../src/NavBar';
 
 
 
 const App = props => {
-  return(
+ return(
 
-    <Router history={browserHistory}>
-      <Route path='/' component={NavBar} >
-        <IndexRoute component={ListingIndexContainer} />
-        <Route path="cereals/:id" component={ListingShowContainer}/>
-      </Route>
-    </Router>
-  )
+   <Router history={browserHistory}>
+     <Route path='/' component={NavBar} >
+       <IndexRoute component={ListingIndexContainer} />
+       <Route path="cereals/:id" component={ListingShowContainer}/>
+     </Route>
+   </Router>
+ )
 }
 
 export default App
