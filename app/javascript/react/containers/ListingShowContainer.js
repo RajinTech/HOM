1d35d9e5aaad3797ced0d86495c31d913bb0a3f9
RajinTech <rajsglass@gmail.com>
2019-02-13 11:13:34 -0500

    fixed photos on show

--- app/javascript/react/containers/ListingShowContainer.js
+++ app/javascript/react/containers/ListingShowContainer.js
@@ -47,15 +47,9 @@ render(){
 
   console.log("pics");
   console.log(this.state.pictures);
-  let picture_gallery = this.state.pictures.map((picture, n) => {
-    return(
 
-
-        <ImageTile
-          key={picture.id}
-          pic={picture.image}
-          />
-
+  let picture_gallery = this.state.pictures.map((picture) => {
+    return( picture.image
         )
       })
   let listing = this.state.listing
@@ -67,36 +61,36 @@ render(){
 
   </div>
        <div className="sidewinder-box">
-        <div className="sidewinder d-10"></div>
-        <div className="sidewinder d-9"></div>
-        <div className="sidewinder d-8"></div>
-        <div className="sidewinder d-7"></div>
-        <div className="sidewinder d-6"></div>
-        <div className="sidewinder d-5"></div>
-        <div className="sidewinder d-4"></div>
-        <div className="sidewinder d-3"></div>
-        <div className="sidewinder d-2"></div>
-        <div className="sidewinder d-1"></div>
-        <div className="sidewinder d1"></div>
-        <div className="sidewinder d2"></div>
-        <div className="sidewinder d3"></div>
-        <div className="sidewinder d4"></div>
-        <div className="sidewinder d5"></div>
-        <div className="sidewinder d6"></div>
-        <div className="sidewinder d7"></div>
-        <div className="sidewinder d8"></div>
-        <div className="sidewinder d9"></div>
-        <div className="sidewinder d10"></div>
-        <div className="sidewinder d11"></div>
-        <div className="sidewinder d12"></div>
-        <div className="sidewinder d13"></div>
-        <div className="sidewinder d14"></div>
-        <div className="sidewinder d15"></div>
+        <div className="sidewinder d-10"><img src={picture_gallery[0]}></img></div>
+      <div className="sidewinder d-9"><img src={picture_gallery[1]}></img></div>
+    <div className="sidewinder d-8"><img src={picture_gallery[2]}></img></div>
+  <div className="sidewinder d-7"><img src={picture_gallery[3]}></img></div>
+<div className="sidewinder d-6"><img src={picture_gallery[4]}></img></div>
+        <div className="sidewinder d-5"><img src={picture_gallery[5]}></img></div>
+      <div className="sidewinder d-4"><img src={picture_gallery[6]}></img></div>
+    <div className="sidewinder d-3"><img src={picture_gallery[7]}></img></div>
+  <div className="sidewinder d-2"><img src={picture_gallery[8]}></img></div>
+<div className="sidewinder d-1"><img src={picture_gallery[9]}></img></div>
+        <div className="sidewinder d1"><img src={picture_gallery[10]}></img></div>
+      <div className="sidewinder d2"><img src={picture_gallery[11]}></img></div>
+    <div className="sidewinder d3"><img src={picture_gallery[12]}></img></div>
+  <div className="sidewinder d4"><img src={picture_gallery[13]}></img></div>
+<div className="sidewinder d5"><img src={picture_gallery[14]}></img></div>
+        <div className="sidewinder d6"><img src={picture_gallery[15]}></img></div>
+      <div className="sidewinder d7"><img src={picture_gallery[16]}></img></div>
+    <div className="sidewinder d8"><img src={picture_gallery[17]}></img></div>
+  <div className="sidewinder d9"><img src={picture_gallery[18]}></img></div>
+<div className="sidewinder d10"><img src={picture_gallery[19]}></img></div>
+        <div className="sidewinder d11"><img src={picture_gallery[20]}></img></div>
+      <div className="sidewinder d12"><img src={picture_gallery[21]}></img></div>
+    <div className="sidewinder d13"><img src={picture_gallery[22]}></img></div>
+  <div className="sidewinder d14"><img src={picture_gallery[23]}></img></div>
+<div className="sidewinder d15"><img src={picture_gallery[24]}></img></div>
 
 
 
       </div>
-      <div className="hexagon" onMouseMove={this.roledex}><button></button></div>
+      <div className="hexagon" onMouseMove={this.roledex}></div>
         <MapShow
          listingsall={this.state.listing}
         />
