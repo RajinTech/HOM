d457f7cf70c4fb6c0fd527c3bab79a71aaa5ce27
RajinTech <rajsglass@gmail.com>
2019-04-04 16:56:07 -0400

    deleted picture factory and tests

--- spec/models/picture_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Picture, type: :model do
-  it 'expect picture to save' do
-    picturebot = FactoryBot.create(:picture)
-    expect(picturebot.present?).to eq(true)
-  end
-end
