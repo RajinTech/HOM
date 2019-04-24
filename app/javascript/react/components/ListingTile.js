9e16ef71719a57214197edc0d0ca9dcb7c5f4a3d
RajinTech <rajsglass@gmail.com>
2019-04-24 10:28:23 -0400

    more styling

--- app/javascript/react/components/ListingTile.js
+++ app/javascript/react/components/ListingTile.js
@@ -5,12 +5,20 @@ const ListingTile = (props) => {
   return (
     <div className="listing_tile">
       <Link to={`/listings/${props.id}`}>
+      <div>
+        <div className='top_half'>
+
           <h8>{props.street} {props.unit}</h8>
           <h8>{props.city}, {props.state}{props.zip}</h8>
+      </div>
+        <div className='bottom_half'>
+
           <h8> <b>Bed:</b> {props.bed}</h8>
           <h8> <b>Bath:</b>  {props.bath}</h8>
           <h8> <b>Sq Ft:</b>  {props.sqft}</h8>
           <h8> <b>Rent:</b>  ${props.rent}</h8>
+      </div>
+      </div>
           <img className="listing_photo" src={props.pic}></img>
       </Link>
     </div>
