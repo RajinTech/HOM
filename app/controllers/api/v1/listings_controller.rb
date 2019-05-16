7be797a3069058f71fb2cf63366aff9da219070b
RajinTech <rajsglass@gmail.com>
2019-05-16 18:04:52 -0400

    fixed naming bug

--- app/controllers/api/v1/listings_controller.rb
+++ app/controllers/api/v1/listings_controller.rb
@@ -69,7 +69,7 @@ class Api::V1::ListingsController < ApiController
   end
 
   def search
-    @listings = Listing.where("name ILIKE ?", "%#{params['search_string']}%")
+    @listings = Listing.where("street ILIKE ?", "%#{params['search_string']}%")
     render json: @listings
   end
 
